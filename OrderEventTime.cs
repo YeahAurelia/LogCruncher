8f168cd59521a92f125abe1acd273d3c81cab5cf
YeahAurelia <aureliadann@outlook.co.nz>
2023-08-16 17:39:44 +1200

    added class for tracking three different values instead of dictionaries 2
    kinda dumb names ima be honest

---
+++ OrderEventTime.cs
@@ -0,0 +1,18 @@
+namespace OETFunctions
+{
+    public struct OrderEventTime
+    {
+        public string EventName;
+        public string EventTime;
+    }
+    public class OET : Dictionary<int, OrderEventTime>
+    {
+        public void Add(int key, string eventLabel, string eventTimeLabel)
+        {
+            OrderEventTime val;
+            val.EventName = eventLabel;
+            val.EventTime = eventTimeLabel;
+            this.Add(key, val);
+        }
+    }
+}
