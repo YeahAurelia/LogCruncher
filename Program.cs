3fec639232fcc7608ec6983809053f3e98da3280
YeahAurelia <aureliadann@outlook.co.nz>
2023-08-16 17:16:41 +1200

    functions to track world events, added notes file, renamed log file for ease
    basically added a new way to track world trigger events and the time they occur.

--- Program.cs
+++ Program.cs
@@ -1,41 +1,78 @@
 ﻿using System;
 using System.IO;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace LogCruncher
 {
     internal class Program
     {
+        int counter =0;
+        Dictionary<string, string> worldEventTimes = new Dictionary<string, string>();
+        List<string> worldEventTypes = new List<string>();
         string currentLine;//the current line in the opened log
         string logToOpen;//the log that must be opened
-        bool complete;//bool for thing
+        bool logCheck;//bool for checking if log loaded and a line can be read
+        bool complete = false;
         static void Main(string[] args)
         {
             Console.WriteLine("Log Cruncher Console");
-            Program p =new Program();
+            Program p = new Program();
             p.start();
         }
         void start()
         {
-            Console.WriteLine("What log is being opened");
-            logToOpen = Console.ReadLine();
-            complete = false;
+            worldEventTypes.Add("Round_Start");
+            worldEventTypes.Add("Round_Overtime");
+            worldEventTypes.Add("Round_Win");
+            worldEventTypes.Add("Round_Length");
+            worldEventTypes.Add("Game_Over");
             while (complete == false)
             {
-                try
+                Console.WriteLine("What log is being opened");
+                logToOpen = Console.ReadLine();
+                logCheck = false;
+                while (logCheck == false)
                 {
-                    using (StreamReader sr = new StreamReader(logToOpen))
+                    try
                     {
-                        while ((currentLine = sr.ReadLine()) != null)
+                        using (StreamReader sr = new StreamReader(logToOpen))
                         {
-                            
+                            currentLine = sr.ReadLine();
+                            while ((currentLine = sr.ReadLine()) != null)
+                            {
+                                ReadLog(currentLine);
+                            }
                         }
                     }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        logCheck = true;
+                    }
+                    logCheck = true;//stops the infinate loop (i am very smaart)
                 }
             }
+        }
+        void ReadLog(string logLine)//reads the line from the log and determines what it is about
+        {
+            if (logLine.Contains("World triggered"))// if true then this indicates a round has begun, ended, entered overtime or won/lost
+            {
+                Console.WriteLine("yeah");
+                TrackWorldEvents(logLine);
+            }
+        }
+        void TrackWorldEvents(string worldLine)//keeps track of world triggers like round start and end and overtime
+        {
+            string eventName = "";
+            for (int i = 0; i < worldEventTypes.Count(); i++)
+            {
+                if (worldLine.Contains(worldEventTypes[i]))
+                {
+                    eventName = worldEventTypes[i];
+                    Console.WriteLine(eventName);
+                    Console.ReadLine();
+                }
+            }
+            worldEventTimes.Add(worldLine.Substring(15, 8), eventName);
         }
     }
 }
